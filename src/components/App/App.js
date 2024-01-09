82e02b4477f322e0f44d473f5415927eae26c94f
Mary Bruff <marybruff5@gmail.com>
2024-01-09 11:21:23 -0700

    react-router initial config

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Welcome from '../Welcome/Welcome';
+import Search from '../Search/Search';
+import Playlist from '../Playlist/Playlist';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome />} /> 
+        <Route path="/search" element={<Search/>} />
+        <Route path="/playlists/:genre" element={<Playlist />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "../src/components/App/App";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
