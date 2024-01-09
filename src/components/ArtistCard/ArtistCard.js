e5aba8b19ac6ca0a6e40ebb24996d79f50dc29e8
Mary Bruff <marybruff5@gmail.com>
2024-01-09 09:35:33 -0700

    initial commit

---
+++ src/components/ArtistCard/ArtistCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ArtistCard.css'
+
+const ArtistCard = () => {
+  return (
+    <div>ArtistCard</div>
+  )
+}
+
+export default ArtistCard
---
+++ src/components/PlaylistCard/PlaylistCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PlaylistCard.css'
+
+const PlaylistCard = () => {
+  return (
+    <div>PlaylistCard</div>
+  )
+}
+
+export default PlaylistCard
