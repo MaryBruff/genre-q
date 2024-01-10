6b00187dd7bf595cfe6e502b9c361e32aaa0cdcc
Mary Bruff <marybruff5@gmail.com>
2024-01-10 16:45:11 -0700

    Basic Swiper carousel functionality

--- src/components/PlaylistCard/PlaylistCard.js
+++ src/components/PlaylistCard/PlaylistCard.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './PlaylistCard.css'
+import NoImage from '../../assets/no-image.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const PlaylistCard = ({ genre, playlist, setPlaylist }) => {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+
+const PlaylistCard = ({ genre, playlist }) => {
+  console.log("PlaylistCard", playlist)
+
+  if (!playlist || playlist.length === 0) {
+    return <p>Loading playlists...</p>; 
+  }
   return (
     <main className='playlist-card'>
-      Cool ass Carousel
+      <section className='playlist-card-container'>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="swiper-container"
+        >
+          {playlist.map((playlist, index) => {
+            const imageUrl = playlist.images[0].url ? playlist.images[0].url : NoImage;
+            return (
+              <SwiperSlide key={index}>
+                <div className='playlist-card-result'>
+                  <a 
+                    href={playlist.external_urls.spotify} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className='playlist-card-result-button'>
+                      Listen on Spotify
+                  </a>
+                  <img className='playlist-card-result-image' src={imageUrl} alt={playlist.name} />
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </section>
       <section className='genre-container'>
         <h2 className='genre-title'>Genre</h2>
         <div className='genre-result-container'>
