797021da137dbd396ea5f0b8eedb32ea761d1904
Mary Bruff <marybruff5@gmail.com>
2024-01-10 11:05:31 -0700

    Welcome component functionality

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Welcome.css'
+import GenreQLogo from '../../assets/GenreLogo.svg'
+import { useNavigate } from 'react-router-dom'
 
 const Welcome = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/search')
+  }
+
   return (
-    <div>Welcome</div>
+    <main className='welcome'>
+      <section className='welcome-container'>
+        <img src={GenreQLogo} style={{ width: '320px', transform: 'translateY(48px)' }} />
+        <h2 className='welcome-title'>Find your next favorite genre</h2>
+        <button onClick={handleClick} className='welcome-button'>Get Started</button>
+      </section>
+    </main>
   )
 }
 
