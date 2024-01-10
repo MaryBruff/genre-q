a26c6acc945e95905799d5e6dfcf8da8834f126e
Mary Bruff <marybruff5@gmail.com>
2024-01-10 11:41:48 -0700

    Genre state updates and redirects onClick

--- src/components/GenreBar/GenreBar.js
+++ src/components/GenreBar/GenreBar.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './GenreBar.css'
+import { useNavigate } from 'react-router-dom/';
 
-const GenreBar = ({ genres }) => {
-  
+const GenreBar = ({ genres, setGenre }) => {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    setGenre(e.target.innerText)
+    navigate(`/playlists/${e.target.innerText}`)
+  }
+
   return (
     <section className='genre-container'>
       <h2 className='genre-title'>Genres</h2>
@@ -10,7 +16,7 @@ const GenreBar = ({ genres }) => {
       {genres.length === 0 ? 
           <p className='genre-result'>No Genres Found.. Try Again</p> :
           genres.map((genre, index) => {
-            return <button key={index} className='genre-result'>{genre}</button>
+            return <button key={index} onClick={handleClick} className='genre-result'>{genre}</button>
           })
         }
       </div>
