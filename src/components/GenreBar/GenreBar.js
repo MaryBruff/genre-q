925ddc957e57ead0704efa46b6a0ac2033e73293
Mary Bruff <marybruff5@gmail.com>
2024-01-11 12:07:38 -0700

    remove getPlaylist func

--- src/components/GenreBar/GenreBar.js
+++ src/components/GenreBar/GenreBar.js
@@ -2,27 +2,11 @@ import React from 'react'
 import './GenreBar.css'
 import { useNavigate } from 'react-router-dom/';
 
-const GenreBar = ({ genres, setGenre, setPlaylist, accessToken }) => {
+const GenreBar = ({ genres, setGenre }) => {
   const navigate = useNavigate();
 
-  const getPlaylist = async (genre) => {
-    const response = await fetch(`https://api.spotify.com/v1/search?type=playlist&limit=10&q=${encodeURIComponent(`genre: ${genre}`)}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
-      }
-    }
-    );
-    const data = await response.json()
-    console.log("GenreBar Button", data.playlists.items)
-    setPlaylist(data.playlists.items)
-  }
-
-  const handleClick = (e) => {
+    const handleClick = (e) => {
     setGenre(e.target.innerText)
-    getPlaylist(e.target.innerText)
     navigate(`/playlists/${e.target.innerText}`)
   }
 
