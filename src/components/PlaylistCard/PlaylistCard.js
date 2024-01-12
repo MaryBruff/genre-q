441f7392c043f7db06ccf54a99a0502089e6f5f9
Mary Bruff <marybruff5@gmail.com>
2024-01-12 14:14:22 -0700

    Add slider alert to PlaylistCard

--- src/components/PlaylistCard/PlaylistCard.js
+++ src/components/PlaylistCard/PlaylistCard.js
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from 'react'
 import './PlaylistCard.css'
 import { useParams } from 'react-router-dom';
 import NoImage from '../../assets/no-image.png'
+import Arrow from '../../assets/arrow-basic.svg'
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards } from 'swiper/modules';
+import { EffectCards, Navigation } from 'swiper/modules';
 
 
 const PlaylistCard = ({ accessToken, genre: propGenre, playlist: propPlaylist }) => {
+  const [hasDragged, setHasDragged] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [playlist, setPlaylist] = useState(propPlaylist || []);
   const [genre, setGenre] = useState(propGenre);
   const { genre: urlGenre } = useParams();
+
+  const handleSlideChange = () => {
+    setHasDragged(true);
+  };
+
+  useEffect(() => {
+    setHasDragged(false);
+  }, [playlist]);
 
   useEffect(() => {
     if (!propPlaylist && urlGenre) {
@@ -23,8 +33,9 @@ const PlaylistCard = ({ accessToken, genre: propGenre, playlist: propPlaylist })
   }, [urlGenre, propGenre, propPlaylist]);
 
   const fetchPlaylists = async (genre) => {
+    const genreQuery = encodeURIComponent(`genre: ${urlGenre}`)
     try {
-      const response = await fetch(`https://api.spotify.com/v1/search?type=playlist&limit=10&q=${encodeURIComponent(`genre: ${genre}`)}`, {
+      const response = await fetch(`https://api.spotify.com/v1/search?type=playlist&limit=10&q=${genreQuery}`, {
         method: 'GET',
         headers: {
           'Authorization': 'Bearer ' + accessToken
@@ -35,6 +46,8 @@ const PlaylistCard = ({ accessToken, genre: propGenre, playlist: propPlaylist })
         setErrorMessage(`No playlists found for genre: ${genre}`);
         setPlaylist([]);
         return;
+      } else if (response.status === 401) {
+        window.location.href = '/expired';
       }
   
       const data = await response.json();
@@ -63,14 +76,24 @@ const PlaylistCard = ({ accessToken, genre: propGenre, playlist: propPlaylist })
       <section className='playlist-card-container'>
         <Swiper
           effect={'cards'}
+          rewind={true}
+          navigation={true}
           grabCursor={true}
-          modules={[EffectCards]}
+          onSlideChange={handleSlideChange}
+          modules={[EffectCards, Navigation]}
           className="swiper-container"
         >
           {playlist.map((playlist, index) => {
             const imageUrl = playlist.images[0].url ? playlist.images[0].url : NoImage;
             return (
               <SwiperSlide key={index}>
+                {!hasDragged && (
+                  <div className="drag-indicator">
+                    {/* Include your arrow SVG and text here */}
+                    <img className='drag-arrow' src={Arrow} alt="Arrow" />
+                    <span>Drag Left</span>
+                  </div>
+                )}
                 <div className='playlist-card-result'>
                   {/* href implementation in case URI link does not redirect */}
                   {/* <a 
